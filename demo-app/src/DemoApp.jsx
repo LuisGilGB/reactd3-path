38c443a918a7919be80aa4466a4d4056fd9b7255
LuisGilGB <luis.gilgb@gmail.com>
2020-08-05 13:25:09 +0200

    Added demo code and dependencies

--- demo-app/src/DemoApp.jsx
+++ demo-app/src/DemoApp.jsx
@@ -1,28 +1,97 @@
-import React from 'react';
-import Path from '../../src/Path';
-import './DemoApp.css';
+import React, { useState } from 'react';
+import JSONTree from 'react-json-tree';
+import Path from '../../Path';
+import { Box, Flex, Button } from 'rebass';
+import Form from './form/Form';
+import { getElOffset, getSvgEl } from './utils';
+import './styles.css';
 
-function DemoApp() {
-    return (
-        <div className="app">
-            <header className="app-header">
-                <p>
-                    Edit <code>src/DemoApp.js</code> and save to reload.
-                </p>
-                <a
-                    className="app-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-            <Path>
-                This is into a Path
-            </Path>
-        </div>
-    );
+export default function App() {
+  const [points, setPoints] = useState([]);
+  const [currentSubPathType, setCurrentSubPathType] = useState('line');
+
+  const addPoint = (newPoint) => {
+    setPoints([...points, newPoint]);
+  };
+
+  const onSvgClick = (e) => {
+    const svgEl = getSvgEl(e.target);
+    if (svgEl) {
+      const offset = getElOffset(svgEl);
+      addPoint({
+        type: currentSubPathType,
+        x: e.pageX - offset.x,
+        y: e.pageY - offset.y
+      });
+    }
+  };
+
+  return (
+    <div className="App">
+      <Flex p={2} overflow="auto">
+        <Box
+          display="inline-block"
+          minWidth={240}
+          width={240}
+          overflow="hidden"
+        >
+          <Box bg="primary" p={2} color="white">
+            Path
+          </Box>
+          <Box p={2}>
+            {points.map((p, i) => (
+              <Box
+                key={i}
+                m={1}
+                sx={{
+                  textAlign: 'left',
+                  flexWrap: 'wrap',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'initial'
+                }}
+              >
+                <JSONTree data={p} hideRoot />
+              </Box>
+            ))}
+          </Box>
+          <Form
+            addPoint={addPoint}
+            type={currentSubPathType}
+            setType={setCurrentSubPathType}
+          />
+        </Box>
+        <Box mx={3} minWidth={600} width={600}>
+          <Box height={600} minHeight={600} sx={{ border: '2px solid black' }}>
+            <svg width={600} height={600} onClick={onSvgClick}>
+              {points && points.length && (
+                <Path
+                  points={points}
+                  stroke="black"
+                  strokeWidth={1}
+                  fill="red"
+                  closePath
+                />
+              )}
+            </svg>
+          </Box>
+          <Flex m={2}>
+            <Button
+              flex={1}
+              bg="secondary"
+              sx={{
+                ':hover': {
+                  bg: 'tomato',
+                  cursor: 'pointer'
+                }
+              }}
+              p={2}
+              onClick={() => setPoints([])}
+            >
+              Reset
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </div>
+  );
 }
-
-export default DemoApp;
---
+++ demo-app/src/form/Form.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Box, Button } from 'rebass';
+import { Select } from '@rebass/forms';
+import NumberField from './NumberField';
+import { OPTIONS } from './consts';
+
+const Form = (props) => {
+  const { type, setType, addPoint } = props;
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(type);
+    addPoint({ type, x, y });
+  };
+
+  const onTypeChange = (e) => {
+    setType(e.target.value);
+  };
+
+  const onXChange = (value) => {
+    setX(value);
+  };
+
+  const onYChange = (value) => {
+    setY(value);
+  };
+
+  return (
+    <Box
+      as="form"
+      display="inline-flex"
+      flexDirection="column"
+      alignItems="stretch"
+      p={1}
+      onSubmit={onSubmit}
+    >
+      <Select name="type" value={type} onChange={onTypeChange}>
+        {OPTIONS.map((opt) => (
+          <option key={opt.key} value={opt.key}>
+            {opt.label}
+          </option>
+        ))}
+      </Select>
+      {type !== 'vertical' && (
+        <NumberField m={2} name="x" label="x" value={x} onChange={onXChange} />
+      )}
+      {type !== 'horizontal' && (
+        <NumberField m={2} name="y" label="y" value={y} onChange={onYChange} />
+      )}
+      <Button
+        m={2}
+        sx={{
+          ':hover': {
+            bg: 'tomato',
+            cursor: 'pointer'
+          }
+        }}
+      >
+        Add
+      </Button>
+    </Box>
+  );
+};
+
+export default Form;
